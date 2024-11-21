30ee1c27d10810448d80410cb1ebb6eb9daff56d
Lee <44310445+lx200916@users.noreply.github.com>
2024-11-21 16:25:47 +0800

    Update Header.

--- app/src/main/cpp/LibHelper.hpp
+++ app/src/main/cpp/LibHelper.hpp
@@ -11,9 +11,9 @@
 #include <android/log.h>
 #include <vector>
 #define TAG "MLLM"
-#define LOGD(...) __android_log_print(ANDROID_LOG_DEBUG,TAG,__VA_ARGS__)
-#define LOGE(...) __android_log_print(ANDROID_LOG_ERROR,TAG,__VA_ARGS__)
-#define LOGI(...) __android_log_print(ANDROID_LOG_INFO,TAG,__VA_ARGS__)
+#define LOGD(...) __android_log_print(ANDROID_LOG_DEBUG, TAG, __VA_ARGS__)
+#define LOGE(...) __android_log_print(ANDROID_LOG_ERROR, TAG, __VA_ARGS__)
+#define LOGI(...) __android_log_print(ANDROID_LOG_INFO, TAG, __VA_ARGS__)
 #include "functional"
 
 namespace mllm {
@@ -22,10 +22,11 @@ namespace mllm {
     class Module;
     class Tensor;
     enum PreDefinedModel {
-        QWEN = 0,
+        QWEN25 = 0,
         FUYU,
         Bert,
-        PhoneLM
+        PhoneLM,
+        QWEN15
     };
 
     enum MLLMBackendType {
@@ -49,13 +50,14 @@ namespace mllm {
 
         // Tokenizer *tokenizer_ = nullptr;
         unsigned int eos_id_ = 2;
-        PreDefinedModel model_ = PreDefinedModel::QWEN;
+        PreDefinedModel model_ = PreDefinedModel::QWEN25;
         MLLMBackendType backend_ = MLLMBackendType::CPU;
         bool is_first_run_cond_ = true;
         int tokens_limit = 4000;
         unsigned postProcessing(std::shared_ptr<Tensor> result, std::shared_ptr<Tensor> &out_result) const;
+
     public:
-        bool setUp(const std::string &base_path, std::string weights_path,  std::string qnn_weights_path, std::string vocab_path, std::string merge_path, PreDefinedModel model, MLLMBackendType backend_type = MLLMBackendType::CPU);
+        bool setUp(const std::string &base_path, std::string weights_path, std::string qnn_weights_path, std::string vocab_path, std::string merge_path, PreDefinedModel model, MLLMBackendType backend_type = MLLMBackendType::CPU);
         void setCallback(callback_t callback);
         void run(std::string &input_str, uint8_t *image, unsigned max_step, unsigned image_length, bool chat_template = false);
         std::vector<float> runForResult(std::string &input_str);
